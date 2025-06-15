6123984cba8afc9dd89c981595925bfe39c8965e
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-06-15 16:09:44 +0100

    preview

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ResumeController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/resume/{user}/preview', [ResumeController::class, 'stream'])->name('resume.stream');
