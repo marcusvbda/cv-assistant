167f6950e7fb7faffd21b8029bdea6318e11a652
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-06-26 13:43:14 +0100

    merge

--- app/Filament/Pages/DefaultResume.php
+++ app/Filament/Pages/DefaultResume.php
@@ -5,20 +5,20 @@ namespace App\Filament\Pages;
 use Filament\Pages\Page;
 use Auth;
 
-class Resume extends Page
+class DefaultResume extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-clipboard-document';
-    protected static ?string $navigationLabel = 'Resume';
+    protected static ?string $navigationLabel = 'Default resume';
     protected static string $view = 'filament.pages.resume';
-    protected static ?string $title = 'Your resume';
+    protected static ?string $title = 'Your default resume';
 
     public $user;
 
     public function getBreadcrumbs(): array
     {
         return [
-            url('/resume') => 'Resume',
-            'generate' => 'Your resume',
+            url('/resume') => 'Default resume',
+            'generate' => 'Your default resume',
         ];
     }
 
