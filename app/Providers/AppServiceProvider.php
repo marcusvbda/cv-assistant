c117f82edeb301e28dd36b90bfba405316b08ee2
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-07-04 21:26:19 +0100

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Assets;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         FilamentAsset::register([
             Assets\Js::make('app', Vite::asset('resources/js/app.js')),
         ]);
